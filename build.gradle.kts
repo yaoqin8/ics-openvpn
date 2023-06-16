c4829f9f897da90520eebad10bfd399bb2205a00
yaoqin <123456789>
2023-06-16 15:51:06 +0800

    改动列表: 1.build.gradle.kts添加阿里云镜像仓库. 2.gradle.properties参数android.enableUnitTestBinaryResources默认就是false，无需显式设置

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,12 @@ buildscript {
 
     kotlin_version = "1.8.20"
     repositories {
+        maven { setUrl("https://maven.aliyun.com/repository/central") }
+        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
+        maven { setUrl("https://jitpack.io") }
         google()
         mavenCentral()
     }
@@ -20,8 +26,13 @@ buildscript {
 
 allprojects {
     repositories {
+        maven { setUrl("https://maven.aliyun.com/repository/central") }
+        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
+        maven { setUrl("https://jitpack.io") }
         google()
         mavenCentral()
-        maven(url = "https://jitpack.io")
     }
 }
